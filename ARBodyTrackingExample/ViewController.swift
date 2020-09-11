1aaffb223022676a5e25a8268e706297cf86ac1b
tokyoyoshida <yoshidaforpublic@gmail.com>
2020-09-12 06:38:23 +0900

    body detection

--- ARBodyTrackingExample/ViewController.swift
+++ ARBodyTrackingExample/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 import RealityKit
+import ARKit
+import SceneKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
+    @IBOutlet var sceneView: ARSCNView!
     
     @IBOutlet var arView: ARView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        initARKit()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        startARKit()
+    }
+
+    fileprivate func initARKit() {
+        sceneView.delegate = self
+    }
+
+    fileprivate func startARKit() {
+        let configuration = ARBodyTrackingConfiguration()
+        sceneView.delegate = self
+        sceneView.session.run(configuration)
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARBodyAnchor {
+            let sphere = SCNNode(geometry: SCNSphere(radius: 1))
+            node.addChildNode(sphere)
+        }
     }
 }
